7eb72a5a260c41b2ad382468e069cfd7912c5706
Albert-Andrei <moldovanualiberta@gmail.com>
2025-02-03 17:53:07 +0200

    feat: new preview image

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 
 // @TODO: update this after first deployment to production
 export const METADATA_URL =
-  process.env.NEXT_PUBLIC_SITE || "https://planable.vercel.app/images/preview.webp";
+  process.env.NEXT_PUBLIC_SITE || "https://planable.vercel.app/images/preview.jpg";
 
 export const DEFAULT_METADATA: Metadata = {
   title: "Media Gallery",
